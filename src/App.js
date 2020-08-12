0cb30d6b000c63a6f428261030e82c99d403c8a2
Jeandilou S. Cimafranca <jeandilou.cimafranca@ripeconcepts.net>
2020-08-12 17:56:13 +0800

    reupdate firebase tutorial

--- src/App.js
+++ src/App.js
@@ -18,13 +18,20 @@ class App extends React.Component {
     }
   }
 
+  unsubscribeFromAuth = null
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user });
 
       console.log(user);
     });
   }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
 
     return (
